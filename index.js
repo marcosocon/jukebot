c3aa391bb3a998434f88b369e2788486a99199c3
marcosocon <oconmarcos97@gmail.com>
2019-11-22 18:14:48 -0300

    small fixes and TODOS added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var bodyParser = require('body-parser')
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 const app = express();
 
 // parse application/x-www-form-urlencoded
@@ -18,38 +18,16 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log('request coming', req.body);
-
-    if(req.body.text === 'play') {
-        const { exec } = require('child_process');
-        const child = exec('spotify play');
-        
-        // use child.stdout.setEncoding('utf8'); if you want text chunks
-        child.stdout.on('data', (chunk) => {
-          // data from standard output is here as buffers
-        });
-        
-        // since these are streams, you can pipe them elsewhere
-        
-        child.on('close', (code) => {
-          console.log(`child process exited with code ${code}`);
-        });
-    }
-    if(req.body.text === 'stop') {
-        const { exec } = require('child_process');
-        const child = exec('spotify stop');
-        
-        // use child.stdout.setEncoding('utf8'); if you want text chunks
-        child.stdout.on('data', (chunk) => {
-          // data from standard output is here as buffers
-        });
-        
-        child.on('close', (code) => {
-          console.log(`child process exited with code ${code}`);
-        });
-    }
-
-    res.send('null');
+  // TODO: si es play solo que haga play, si tiene texto despues que busque ese texto de despues osea /jkb play easy lover, manda spotify play easy lover :)
+  if(req.body.text === 'play') {
+    exec('spotify play');
+  }
+  // esto queda igual, el stop se mantiene
+  if(req.body.text === 'stop') {
+    exec('spotify stop');
+  }
+  // TODO: envia un mensaje para que aparezca :)
+  res.send('null');
 });
 
 
